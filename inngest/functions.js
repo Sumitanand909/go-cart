b267ee2754c6c8ad785ff8d308728be17c64d7bd
Sumit <sumitanand28.in@gmail.com>
2025-10-31 22:12:13 +0530

    updated

---
+++ inngest/functions.js
@@ -0,0 +1,50 @@
+import {inngest} from './client'
+import prisma from '@/lib/prisma'
+
+// inngest function to save user data to a db
+export const syncUserCreation = inngest.createFunction(
+    {id: 'sync-user-create'},
+    {event: 'clerk/user.created'},
+    async({event}) =>{
+        const {data} = event
+        await prisma.user.create({
+            data: {
+                id: data.id,
+                email: data.email_addresses[0].email_address,
+                name: `${data.first_name} ${data.last_name}`,
+                image: data.image_url
+            }
+        })
+    }
+)
+
+
+// inngest function to update user data in db
+export const syncUserUpdation = inngest.createFunction(
+    {id: 'sync-user-update'},
+    {event: 'clerk/user.updated'},
+    async({event}) =>{
+        const {data} = event
+        await prisma.user.update({
+            where: {id: data.id},
+            data: {
+                email: data.email_addresses[0].email_address,
+                name: `${data.first_name} ${data.last_name}`,
+                image: data.image_url
+            }
+        })
+    }
+)
+
+
+// inngest function to delete user data form db
+export const syncUserDeletion = inngest.createFunction(
+    {id: 'sync-user-delete'},
+    {event: 'clerk/user.deleted'},
+    async({event}) =>{
+        const {data} = event
+        await prisma.user.delete({
+            where: {id: data.id}
+        })
+    }
+)
